271d910d8e691db171dd28e897914bb0269848d6
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-01-30 18:32:48 -0600

    It work's

--- form_upload.php
+++ form_upload.php
@@ -1,4 +1,4 @@
 <form action="recibe.php" method="post" enctype="multipart/form-data">
-  <input type="file" name="archivo"><br />
+  <input type="file" name="file"><br />
   <input type="submit" name="enviar" value="Enviar">
 </form>
--- recibe.php
+++ recibe.php
@@ -6,11 +6,11 @@ $_FILES['file']['size']; //Tamaño en bytes
 $_FILES['file']['type']; //Tipo del archivo
 $_FILES['file']['error']; //Error devuelto al subir el archivo
 
-$tam_max = 1048576;
+$tam_max = 10485760;
 if(is_uploaded_file($_FILES['file']['tmp_name'])) {
     if($_FILES['file']['size'] > $tam_max) {
          unlink($_FILES['file']['tmp_name']);
-         echo("El archivo es demasiado grande");
+         echo("<script> alert ('El archivo es demasiado grande') </script>");
     } else {
          if($_FILES['file']['type'] == "image/gif"
          OR $_FILES['file']['type'] == "image/pjpeg"
@@ -19,19 +19,18 @@ if(is_uploaded_file($_FILES['file']['tmp_name'])) {
          OR eregi("(.php3)$", $_FILES['file']['name'])
          OR eregi("(.php4)$", $_FILES['file']['name'])
          OR eregi("(.phtml)$", $_FILES['file']['name'])) {
-              echo ("Tipo de archivo no permitido");
+              echo ("<script> alert ('Tipo de archivo no permitido') </script>");
               unlink($_FILES['file']['tmp_name']);
          } else {
               if(strstr($_FILES['file']['name'], '..')) {
                    echo ("Acción no permitida");
               } else {
-                   echo ("El archivos se subio de manera correcta");
+                   echo ("<script> alert ('El archivos se subio de manera correcta') </script>");
                    move_uploaded_file($_FILES['file']['tmp_name'], "/var/www/uploads/{$_FILES['file']['name']}");
                    unlink($_FILES['file']['tmp_name']);
               }
          }
     }
 } else {
-    echo ("Ha ocurrido un error al subir el archivo");
-    //Comprobar $_FILES['file']['error']
+    echo ("<script> alert ('Ha ocurrido un error al subir el archivo') </script>");
 } ?>
